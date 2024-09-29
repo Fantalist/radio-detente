aeb70f20023a063a0f171ce726a4fd0dfa30d5af
Fantalist <ruby231@outlook.fr>
2024-09-29 16:13:51 +0200

    Site

---
+++ listen.pls
@@ -0,0 +1,3 @@
+[playlist]
+NumberOfEntries=1
+File1=http://78.129.229.122:23487/
---
+++ player/config/config.js
@@ -0,0 +1,5 @@
+var g = '78.129.132.7'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
+var h = '23215'; // PORT - Radionomy : 80
+var z = '/;stream.mp3'; //Shoutcast='/;stream.mp3'
+var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
+var l = ''; // Radionomy GUID (For Current Listen)
